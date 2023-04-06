c1c21a93467698b2a54e8ecd6a834b7c1894a2a2
Ana Campos <arcampos171@gmail.com>
2023-04-06 04:09:33 -0700

    created homepage and modified server.js file and readme.md template

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
-const PORT = 3001;
+const exphbs = require('express-handlebars');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+// Set up Handlebars as the view engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Use the "public" folder for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the homepage and render the "home" template
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
