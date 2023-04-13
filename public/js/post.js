470c655621a624e8c54ce3ac1ab0b4721a857b71
HarisM <harismahjoob42@gmail.com>
2023-04-13 15:19:12 -0700

    working on posts

--- public/js/post.js
+++ public/js/post.js
@@ -1,25 +1,46 @@
-const newPostHandler = async (event) => {
+const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const title = document.querySelector("#title").value.trim();
-    const post_content = document.querySelector("#post-content").value.trim();
-  
+    const title = document.querySelector('#post-title').value.trim();
+    const post_content = document.querySelector('#post-content').value.trim();
+    
     if (title && post_content) {
-      const response = await fetch("/api/posts", {
-        method: "POST",
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
         body: JSON.stringify({ title, post_content }),
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const responseData = await response.json();
+  
+      // Redirect to new post
+      window.location.href=`/posts/${responseData.id}`;
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('user-id')) {
+      const id = event.target.getAttribute('user-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
       });
   
       if (response.ok) {
-        document.location.replace("/dashboard");
+        document.location.replace('/profile');
       } else {
-        alert("Failed to create post");
+        alert('Failed to delete post');
       }
     }
   };
   
   document
-    .querySelector("#new-post-form")
-    .addEventListener("submit", newPostHandler);
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);
   
